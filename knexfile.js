4fb15dddf6d0d9d02ca54120bf42e2d09b3cba8d
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-06-11 13:45:03 +0900

    first commit

---
+++ knexfile.js
@@ -0,0 +1,56 @@
+// Update with your config settings.
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./db/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./db/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./db/migrations",
+      tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+};
